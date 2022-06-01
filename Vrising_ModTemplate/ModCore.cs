3f1feff4a4c738f205f01bd8799201b00d7fc139
sbtoonz <sbtoonz@gmail.com>
2022-06-01 07:54:45 -0600

    Base commit with nugets
    Signed-off-by: sbtoonz <sbtoonz@gmail.com>

---
+++ Vrising_ModTemplate/ModCore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BepInEx;
+using BepInEx.IL2CPP;
+
+namespace TestVrising
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Class1 : BasePlugin
+    {
+        public override void Load()
+        {
+            
+        }
+    }
+    
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "Vrising_Mod";
+        public const string PLUGIN_NAME = "Vrising_Mod";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
